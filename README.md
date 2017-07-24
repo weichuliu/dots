bb94ffaeab36c03a3dedc7b06f1f020b6b843564
Weichu Liu <typhoonsword@gmail.com>
2017-07-24 15:08:13 -0500

    Fix ipython annoying key bindings

--- README.md
+++ README.md
@@ -57,6 +57,16 @@ The `iterm2_shell_integration` downloaded from [here](https://iterm2.com/shell_i
 
 It is a cleaner bash-preexec version, to solve [#5787](https://gitlab.com/gnachman/iterm2/issues/5787).
 
+#### IPython 5.0+ "Option(Alt) + Arrows"
+
+If you are using `ipython` and find `Option(Alt) + Up/Down/Left/Right` throwing `[1;9A` ish code, do the following:
+
+```
+dots $ ln -s "$(pwd)/ipython_startup_keybindings.py" ~/.ipython/profile_default/startup/
+```
+
+to have ipython correctly bind the sequence to word jump / history search.
+
 ## Other files
 
 - brew_list: Softwares I installed with Homebrew
---
+++ ipython_startup_keybindings.py
@@ -0,0 +1,30 @@
+# This is the keybinding to fix "Option(Alt) + Arrowkeys" under iTerm2's xterm Key Preset.
+# Since from IPython 5+, it uses pyreadline and doesn't honor inputrc anymore.
+# To activate this, link this file under ~/.ipython/profile_default/startup/
+# This is tested under IPython 6.0 with Python 3.6
+
+from IPython import get_ipython
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.key_binding.bindings.named_commands import get_by_name
+____ip = get_ipython()
+# print(ip)
+if hasattr(____ip, 'pt_cli'):
+    # Meta + ^
+    ____ip.pt_cli.application.key_bindings_registry.add_binding(
+        Keys.Escape, *'[1;9A',
+        )(get_by_name('previous-history'))
+    # Meta + v
+    ____ip.pt_cli.application.key_bindings_registry.add_binding(
+        Keys.Escape, *'[1;9B',
+        )(get_by_name('next-history'))
+    # Meta + ->
+    ____ip.pt_cli.application.key_bindings_registry.add_binding(
+        Keys.Escape, *'[1;9C',
+        )(get_by_name('forward-word'))
+    # Meta + <-
+    ____ip.pt_cli.application.key_bindings_registry.add_binding(
+        Keys.Escape, *'[1;9D',
+        )(get_by_name('backward-word'))
+
+del get_ipython, Keys, get_by_name
+del ____ip
